554b7501949b45b7a499705143dfb7be38294e9b
ervin <ervin.cabascango.s@gmail.com>
2021-05-08 23:59:49 -0500

    feat: login Auth0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+
+import { CallbackComponent } from './components/callback/callback.component';
+import { HomeComponent } from './components/home/home.component';
+import { PriceComponent } from './components/price/price.component';
+import { ProtectedComponent } from './components/protected/protected.component';
+
+import { AuthGuard } from './services/auth.guard';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'price', component: PriceComponent },
+  { 
+    path: 'protected', 
+    component: ProtectedComponent,
+    canActivate: [AuthGuard]
+   },
+  { path: 'callback', component: CallbackComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' },]
+  ;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProtectedComponent } from './components/protected/protected.component';
+import { PriceComponent } from './components/price/price.component';
+import { AuthModule } from '@auth0/auth0-angular';
+import { CallbackComponent } from './components/callback/callback.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProtectedComponent,
+    PriceComponent,
+    CallbackComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-1k3cx5kg.us.auth0.com',
+      clientId: 'XpnrsOTQ6Pi6dPAx0pqSqQCJakFLnorn'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
